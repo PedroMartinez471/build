7576a40ea86072b0f8cd926c54aebae172a64799
pedro <peri.b182@gmail.com>
2020-03-08 19:18:45 +0100

    build

---
+++ build/precache-manifest.10e538f185ac420398d8cc5c36cf1f48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "951b5420469adabc48d0e7c8b55d3412",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ae90b2f1c1ee3743f38",
+    "url": "/static/css/2.a00eef2d.chunk.css"
+  },
+  {
+    "revision": "c0b1f78f318c8fe85d72",
+    "url": "/static/css/main.a76a9fb2.chunk.css"
+  },
+  {
+    "revision": "0ae90b2f1c1ee3743f38",
+    "url": "/static/js/2.9e3fe539.chunk.js"
+  },
+  {
+    "revision": "edfc1f2bdfcad1767fef8b6ee0f470c5",
+    "url": "/static/js/2.9e3fe539.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0b1f78f318c8fe85d72",
+    "url": "/static/js/main.06deb135.chunk.js"
+  },
+  {
+    "revision": "e92c818309134516c215",
+    "url": "/static/js/runtime-main.25db6db8.js"
+  }
+]);
